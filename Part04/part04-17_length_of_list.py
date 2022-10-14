6bec0340b75a7cc280f978dc609d54789996e131
Simona Gi <114916203+Simonagi85@users.noreply.github.com>
2022-10-14 10:55:13 +0200

    Add files via upload

---
+++ Part04/part04-17_length_of_list.py
@@ -0,0 +1,8 @@
+# Write your solution here
+def length(list):
+    return len(list)
+
+
+my_list = [3, 6, -4, 6, 8]
+result = length(my_list)
+print(result)
---
+++ Part04/part04-18_mean.py
@@ -0,0 +1,11 @@
+# Write your solution here
+import statistics
+
+
+def mean(my_list: list):
+    return statistics.mean(my_list)
+
+
+my_list = [3, 6, -4]
+result = mean(my_list)
+print(result)
