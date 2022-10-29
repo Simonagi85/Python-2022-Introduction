a6442dbf86c3207783daff3ee1cf644764a951c6
Simona Gi <114916203+Simonagi85@users.noreply.github.com>
2022-10-29 21:00:45 +0200

    Add files via upload

---
+++ Part04/part04-32_integers_to_strings.py
@@ -0,0 +1,12 @@
+# Write your solution here
+def formatted(my_list: list):
+    result = []
+
+    for item in my_list:
+        result.append(f"{item:.2f}")
+    return result
+
+
+my_list = [1.234, 0.3333, 0.11111, 3.446]
+new_list = formatted(my_list)
+print(new_list)
